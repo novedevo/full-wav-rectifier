e082c61100bae0f75a509afb3af5072da28f48dc
Devon Sawatsky <devon@nove.dev>
2025-01-04 08:29:32 +0000

    fix numerous bugs with mul_by and div_by

--- repiquemos-cli/src/main.rs
+++ repiquemos-cli/src/main.rs
@@ -1,5 +1,5 @@
+use hound::{WavSpec, WavWriter};
 use repiquemos::*;
-use hound::WavWriter;
 
 const CORRUPTED_ERROR: &str = "corrupted wav file: sample bit depth doesn't match header or floats aren't within -1.0 and 1.0";
 
@@ -7,73 +7,95 @@ fn main() {
     let input_file = std::env::args()
         .nth(1)
         .unwrap_or("data/input.wav".to_string()); //.expect("No input file provided");
-    let reader = hound::WavReader::open(input_file).expect("Failed to open input file");
+    let mut reader = hound::WavReader::open(input_file).expect("Failed to open input file");
     let spec = reader.spec();
 
-    let samples: Box<dyn Iterator<Item = f64>> = if spec.sample_format == hound::SampleFormat::Int {
-        let iter = reader.into_samples::<i32>().map(|s| {
-            s.expect(CORRUPTED_ERROR) as f64 / 2_i32.pow(spec.bits_per_sample as u32) as f64
-        });
-        Box::new(iter)
-    } else {
-        let iter = reader.into_samples::<f32>().map(|s| {
-            s.and_then(|f| {
-                if f >= -1.0 && f <= 1.0 {
-                    Ok(f)
-                } else {
-                    Err(hound::Error::TooWide)
-                }
-            })
-            .expect(CORRUPTED_ERROR) as f64
-        });
-        Box::new(iter)
-    };
+    let mut samples: Box<dyn Iterator<Item = f64>> =
+        if spec.sample_format == hound::SampleFormat::Int {
+            let iter = reader.samples::<i32>().map(|s| {
+                s.expect(CORRUPTED_ERROR) as f64 / 2_i32.pow(spec.bits_per_sample as u32) as f64
+            });
+            Box::new(iter)
+        } else {
+            let iter = reader.samples::<f32>().map(|s| {
+                s.and_then(|f| {
+                    if f >= -1.0 && f <= 1.0 {
+                        Ok(f)
+                    } else {
+                        Err(hound::Error::UnfinishedSample)
+                    }
+                })
+                .expect(CORRUPTED_ERROR) as f64
+            });
+            Box::new(iter)
+        };
 
     let dsl = std::env::args().nth(3).unwrap();
     let dsl = dsl.split(",");
 
-    let mut operating: Box<dyn Iterator<Item = f64>> = Box::new(samples.into_iter());
     for command in dsl {
         match command {
             "skipclip" => {
-                operating = Box::new(operating.filter_map(|s| skipclip(s, -3.0)));
+                samples = Box::new(samples.filter_map(|s| skipclip(s, -3.0)));
             }
             "rectify" => {
-                operating = Box::new(operating.map(rectify));
+                samples = Box::new(
+                    samples
+                        .map(rectify)
+                        .map(|s| dc_offset(s, -0.5))
+                        .map(|s| ring_mod(s, 2.0)),
+                );
             }
             "accumulate" => {
                 let mut accumulator = 0.0f64;
-                operating = Box::new(operating.map(move |sample| {
+                samples = Box::new(samples.map(move |sample| {
                     let (sample, newacc) = acc(sample, accumulator);
                     accumulator = newacc;
                     sample
                 }))
             }
-            "mul-by-previous" | "div-by-previous" => {
+            "mul-by-previous" => {
                 let mut prev = 1.0;
-                let op = if command == "mul_by_previous" {
-                    mul_by_previous
-                } else {
-                    div_by_previous
-                };
-                operating = Box::new(operating.map(move |sample| {
-                    let retval = op(sample, prev);
+                samples = Box::new(samples.map(move |sample| {
+                    let retval = safe_mul_by_previous(sample, prev);
                     prev = sample;
                     retval
                 }))
             }
+            "div-by-previous" => {
+                let mut prev = 1.0;
+                samples = Box::new(
+                    samples
+                        .filter_map(skip_silence)
+                        .map(move |sample| {
+                            let retval: f64 = div_by_previous(sample, prev);
+                            prev = sample;
+                            retval
+                        })
+                        .map(|s| dc_offset(s, -1.0))
+                        .map(clamp),
+                )
+            }
             _ => unimplemented!("no support for this operation yet"),
         }
     }
-
     let mut writer =
         WavWriter::create("data/output.wav", spec).expect("couldn't create output file");
-    for sample in operating {
+    let samples = samples.collect::<Vec<_>>();
+    for sample in samples {
         if spec.sample_format == hound::SampleFormat::Int {
-            let sample = (sample * 2_i32.pow(spec.bits_per_sample as u32 - 1) as f64) as i32;
-            writer.write_sample(sample).expect("IO error");
+            let inted_sample =
+                (sample * (2_i32.pow(spec.bits_per_sample as u32 - 1) - 1) as f64) as i32;
+            if let Err(e) = writer.write_sample(inted_sample) {
+                panic!(
+                    "IO error: writing float sample: {sample} (inted: {inted_sample}): {:?}: {:#}",
+                    e, e
+                )
+            }
         } else {
-            writer.write_sample(sample as f32).expect("IO error");
+            writer
+                .write_sample(sample as f32)
+                .expect("IO error: writing f32 sample");
         }
     }
     writer.flush().expect("IO error");
--- repiquemos/src/lib.rs
+++ repiquemos/src/lib.rs
@@ -1,5 +1,15 @@
+use core::f64;
+
 pub fn rectify(sample: f64) -> f64 {
-    (sample.abs() - 0.5) * 2.0
+    sample.abs()
+}
+
+pub fn dc_offset(sample: f64, offset: f64) -> f64 {
+    sample + offset
+}
+
+pub fn ring_mod(sample: f64, amplification: f64) -> f64 {
+    sample * amplification
 }
 
 pub fn skipclip(sample: f64, threshold_db: f64) -> Option<f64> {
@@ -11,21 +21,39 @@ pub fn skipclip(sample: f64, threshold_db: f64) -> Option<f64> {
     }
 }
 
+pub fn skip_silence(sample: f64) -> Option<f64> {
+    if sample == 0.0 {
+        None
+    } else {
+        Some(sample)
+    }
+}
+
 pub fn acc(sample: f64, accumulator: f64) -> (f64, f64) {
     let acc = (accumulator + sample.abs()) % 2.0;
     (acc - 1.0, acc)
 }
 
+//author: gen@nyble.dev
+pub fn safe_mul_by_previous(sample: f64, prev: f64) -> f64 {
+    let retval = sample.abs() * prev.abs();
+    if sample.is_sign_positive() {
+        retval
+    } else {
+        -retval
+    }
+}
+
 pub fn mul_by_previous(sample: f64, prev: f64) -> f64 {
-    (sample * prev - 0.5) * 2.0
+    sample * prev
 }
 
 pub fn div_by_previous(sample: f64, prev: f64) -> f64 {
-    if prev != 0.0 {
-        (sample / prev).clamp(-1.0, 1.0)
-    } else {
-        sample
-    }
+    sample / prev
+}
+
+pub fn clamp(sample: f64) -> f64 {
+    sample.clamp(-1.0, 1.0)
 }
 
 #[inline(always)]
