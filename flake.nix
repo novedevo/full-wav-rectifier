2c4039f96e6eeb5567679816d938ee3fa1056fd4
Devon Sawatsky <devon@nove.dev>
2025-02-28 06:24:07 +0000

    aah working flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  # inspired heavily by xe iaso's go template
+  # inspired heavily by xe iaso's go template and https://dev.to/misterio/how-to-package-a-rust-app-using-nix-3lh3
   description = "meow";
 
   inputs = {
@@ -8,11 +8,24 @@
 
   outputs = { self, nixpkgs }:
 
-    let version = builtins.substring 0 8 self.lastModifiedDate;
-    in  {
+  let 
+    version = builtins.substring 0 8 self.lastModifiedDate;
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  in  {
 
-    
-
+    packages = forAllSystems (system:
+      let pkgs = nixpkgsFor.${system}; in
+      {
+        default = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "repiquemos";
+          inherit version;
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      }
+    );
 
   };
 }
