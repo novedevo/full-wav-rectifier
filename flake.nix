84c582cbe8f97d7559f14d108b81c8a7e7167c1d
Devon Sawatsky <devon@nove.dev>
2025-03-01 00:33:59 +0000

    more flake tweaks

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
   outputs = { self, nixpkgs }:
 
   let 
-    version = builtins.substring 0 8 self.lastModifiedDate; # e.g. 20240228 for Feb 28th 2024
 
     # build matrix
     arch = ["x86_64" "aarch64"];
@@ -18,15 +17,17 @@
 
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems; # partial application :3
     nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
   in  {
     packages = forAllSystems (system:
       let pkgs = nixpkgsFor.${system}; in
       {
-        default = pkgs.rustPlatform.buildRustPackage rec {
+        default = pkgs.rustPlatform.buildRustPackage {
           pname = "repiquemos";
-          inherit version;
           src = pkgs.lib.cleanSource ./.;
           cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ clippy ];
+          buildInputs = [ clippy ];
         };
       }
     );
@@ -38,20 +39,23 @@
       };
     });
 
-    devShells = forAllSystems (system: 
-      let pkgs = nixpkgsFor.${system};
-      in {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustc 
-            rust-analyzer 
-            rustfmt 
-            vscode-extensions.rust-lang.rust-analyzer
-            clippy
-          ];
-        };
-      }
-      );
+    # devShells = forAllSystems (system: 
+    #   let 
+    #     pkgs = nixpkgsFor.${system};
+    #   in {
+    #     default = pkgs.mkShell {
+    #       # https://nixos.wiki/wiki/Rust#Shell.nix_example
+    #       RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    #       buildInputs = with pkgs; [
+    #         rustc 
+    #         rust-analyzer 
+    #         rustfmt 
+    #         vscode-extensions.rust-lang.rust-analyzer
+    #         clippy
+    #       ];
+    #     };
+    #   }
+    #   );
 
   };
 }
