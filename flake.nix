151e04b2da69b96630933cca79a115afead8b99d
Devon Sawatsky <devon@nove.dev>
2025-03-04 01:02:43 +0000

    simple cross compiling?

--- flake.nix
+++ flake.nix
@@ -3,57 +3,39 @@
   description = "meow";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";    
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, fenix, nixpkgs }:
-
-  let 
-
-    # build matrix
-    arch = ["x86_64" "aarch64"];
-    kernel = ["linux" "darwin"];
-    supportedSystems = nixpkgs.lib.mapCartesianProduct (x: "${x.arch}-${x.kernel}") {inherit arch kernel;}; # hehe cartesian product
-
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems; # partial application :3
-    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-  in  {
-    # packages = forAllSystems (system:
-    #   let pkgs = nixpkgsFor.${system}; in
-    #   {
-    #     default = pkgs.rustPlatform.buildRustPackage {
-    #       pname = "repiquemos";
-    #       src = pkgs.lib.cleanSource ./.;
-    #       cargoLock.lockFile = ./Cargo.lock;
-    #       nativeBuildInputs = [ pkgs.clippy ];
-    #       buildInputs = [ pkgs.clippy ];
-    #     };
-    #   }
-    # );
-
-    # apps = forAllSystems (system: {
-    #   default = {
-    #     type = "app";
-    #     program = "${self.packages.${system}.default}/bin/repiquemos";
-    #   };
-    # });
-
-    devShells = forAllSystems (system: 
-      let 
-        pkgs = nixpkgsFor.${system};
-      in {
-        default = pkgs.mkShell {
-          nativeBuildInputs = [
-            fenix.packages.${system}.stable.toolchain
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = 
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          target = "aarch64-unknown-linux-gnu";
+          toolchain = with fenix.packages.${system}; combine [
+            minimal.cargo
+            minimal.rustc
+            targets.${target}.latest.rust-std
           ];
+        in (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildPackage {
+          src = ./.;
+          CARGO_BUILD_TARGET = target;
+          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = 
+            let
+              inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
+            in "${cc}/bin/${cc.targetPrefix}cc";
         };
-      }
-      );
-
-  };
+    });
 }
